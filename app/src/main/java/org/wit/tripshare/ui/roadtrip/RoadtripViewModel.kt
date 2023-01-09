73e5b7183f95e53da4050ca2face5a575d5e0b9d
Dan Coffey <dcoffey90@gmail.com>
2023-01-09 17:58:55 +0000

    Completed implementation of profile image management on Firebase and in-app.

--- app/src/main/java/org/wit/tripshare/ui/roadtrip/RoadtripViewModel.kt
+++ app/src/main/java/org/wit/tripshare/ui/roadtrip/RoadtripViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseUser
 import org.wit.tripshare.firebase.FirebaseDBManager
+import org.wit.tripshare.firebase.FirebaseImageManager
 import org.wit.tripshare.models.RoadtripModel
 
 class RoadtripViewModel : ViewModel() {
@@ -19,7 +20,7 @@ class RoadtripViewModel : ViewModel() {
         roadtrip: RoadtripModel
     ) {
         status.value = try {
-            //roadtrip.profilepic = FirebaseImageManager.imageUri.value.toString()
+            roadtrip.profilepic = FirebaseImageManager.imageUri.value.toString()
             FirebaseDBManager.create(firebaseUser, roadtrip)
             true
         } catch (e: IllegalArgumentException) {
